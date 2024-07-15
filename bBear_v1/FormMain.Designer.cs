a65b926412cec10870362d641f215bb09f644a23
ben dcemb <bendcemb@gmail>
2024-07-15 16:58:14 +0700

    1

---
+++ bBear_v1/FormMain.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace bBear_v1
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnSetting = new System.Windows.Forms.Button();
+            this.btnReport = new System.Windows.Forms.Button();
+            this.btnInvoice = new System.Windows.Forms.Button();
+            this.btnRecipt = new System.Windows.Forms.Button();
+            this.btnFile = new System.Windows.Forms.Button();
+            this.btnRecord = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnDashboard = new System.Windows.Forms.Button();
+            this.userControlFile1 = new bBear_v1.UserControlFile();
+            this.userControlRecord1 = new bBear_v1.UserControlRecord();
+            this.panel1.SuspendLayout();
+            this.panel8.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.panel8);
+            this.panel1.Controls.Add(this.panel6);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel7);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel5);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 611);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel8
+            // 
+            this.panel8.Controls.Add(this.btnSetting);
+            this.panel8.Location = new System.Drawing.Point(3, 506);
+            this.panel8.Name = "panel8";
+            this.panel8.Padding = new System.Windows.Forms.Padding(10, 0, 0, 120);
+            this.panel8.Size = new System.Drawing.Size(194, 63);
+            this.panel8.TabIndex = 6;
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.btnReport);
+            this.panel6.Location = new System.Drawing.Point(3, 437);
+            this.panel6.Name = "panel6";
+            this.panel6.Padding = new System.Windows.Forms.Padding(10, 0, 0, 120);
+            this.panel6.Size = new System.Drawing.Size(194, 63);
+            this.panel6.TabIndex = 5;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.btnInvoice);
+            this.panel4.Location = new System.Drawing.Point(3, 299);
+            this.panel4.Name = "panel4";
+            this.panel4.Padding = new System.Windows.Forms.Padding(10, 0, 0, 120);
+            this.panel4.Size = new System.Drawing.Size(194, 63);
+            this.panel4.TabIndex = 3;
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.btnRecipt);
+            this.panel7.Location = new System.Drawing.Point(3, 368);
+            this.panel7.Name = "panel7";
+            this.panel7.Padding = new System.Windows.Forms.Padding(10, 0, 0, 120);
+            this.panel7.Size = new System.Drawing.Size(194, 63);
+            this.panel7.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.btnFile);
+            this.panel3.Location = new System.Drawing.Point(3, 161);
+            this.panel3.Name = "panel3";
+            this.panel3.Padding = new System.Windows.Forms.Padding(10, 0, 0, 120);
+            this.panel3.Size = new System.Drawing.Size(194, 63);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.btnRecord);
+            this.panel5.Location = new System.Drawing.Point(3, 230);
+            this.panel5.Name = "panel5";
+            this.panel5.Padding = new System.Windows.Forms.Padding(10, 0, 0, 120);
+            this.panel5.Size = new System.Drawing.Size(194, 63);
+            this.panel5.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnDashboard);
+            this.panel2.Location = new System.Drawing.Point(3, 92);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(10, 0, 0, 120);
+            this.panel2.Size = new System.Drawing.Size(194, 63);
+            this.panel2.TabIndex = 0;
+            // 
+            // btnSetting
+            // 
+            this.btnSetting.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSetting.FlatAppearance.BorderSize = 0;
+            this.btnSetting.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSetting.ForeColor = System.Drawing.Color.Black;
+            this.btnSetting.Image = global::bBear_v1.Properties.Resources.process32;
+            this.btnSetting.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSetting.Location = new System.Drawing.Point(10, 0);
+            this.btnSetting.Name = "btnSetting";
+            this.btnSetting.Size = new System.Drawing.Size(184, 63);
+            this.btnSetting.TabIndex = 0;
+            this.btnSetting.Text = "Setting";
+            this.btnSetting.UseVisualStyleBackColor = true;
+            // 
+            // btnReport
+            // 
+            this.btnReport.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnReport.FlatAppearance.BorderSize = 0;
+            this.btnReport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReport.ForeColor = System.Drawing.Color.Black;
+            this.btnReport.Image = global::bBear_v1.Properties.Resources.frequency32;
+            this.btnReport.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnReport.Location = new System.Drawing.Point(10, 0);
+            this.btnReport.Name = "btnReport";
+            this.btnReport.Size = new System.Drawing.Size(184, 63);
+            this.btnReport.TabIndex = 0;
+            this.btnReport.Text = "Report";
+            this.btnReport.UseVisualStyleBackColor = true;
+            // 
+            // btnInvoice
+            // 
+            this.btnInvoice.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnInvoice.FlatAppearance.BorderSize = 0;
+            this.btnInvoice.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInvoice.ForeColor = System.Drawing.Color.Black;
+            this.btnInvoice.Image = global::bBear_v1.Properties.Resources.Invoice321;
+            this.btnInvoice.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnInvoice.Location = new System.Drawing.Point(10, 0);
+            this.btnInvoice.Name = "btnInvoice";
+            this.btnInvoice.Size = new System.Drawing.Size(184, 63);
+            this.btnInvoice.TabIndex = 0;
+            this.btnInvoice.Text = "Invoice";
+            this.btnInvoice.UseVisualStyleBackColor = true;
+            // 
+            // btnRecipt
+            // 
+            this.btnRecipt.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnRecipt.FlatAppearance.BorderSize = 0;
+            this.btnRecipt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRecipt.ForeColor = System.Drawing.Color.Black;
+            this.btnRecipt.Image = global::bBear_v1.Properties.Resources.pay32;
+            this.btnRecipt.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRecipt.Location = new System.Drawing.Point(10, 0);
+            this.btnRecipt.Name = "btnRecipt";
+            this.btnRecipt.Size = new System.Drawing.Size(184, 63);
+            this.btnRecipt.TabIndex = 0;
+            this.btnRecipt.Text = "Recipt";
+            this.btnRecipt.UseVisualStyleBackColor = true;
+            // 
+            // btnFile
+            // 
+            this.btnFile.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnFile.FlatAppearance.BorderSize = 0;
+            this.btnFile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFile.ForeColor = System.Drawing.Color.Black;
+            this.btnFile.Image = global::bBear_v1.Properties.Resources.folders32;
+            this.btnFile.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnFile.Location = new System.Drawing.Point(10, 0);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(184, 63);
+            this.btnFile.TabIndex = 0;
+            this.btnFile.Text = "File";
+            this.btnFile.UseVisualStyleBackColor = true;
+            this.btnFile.Click += new System.EventHandler(this.btnFile_Click);
+            // 
+            // btnRecord
+            // 
+            this.btnRecord.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnRecord.FlatAppearance.BorderSize = 0;
+            this.btnRecord.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRecord.ForeColor = System.Drawing.Color.Black;
+            this.btnRecord.Image = global::bBear_v1.Properties.Resources.stats32;
+            this.btnRecord.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRecord.Location = new System.Drawing.Point(10, 0);
+            this.btnRecord.Name = "btnRecord";
+            this.btnRecord.Size = new System.Drawing.Size(184, 63);
+            this.btnRecord.TabIndex = 0;
+            this.btnRecord.Text = "Record";
+            this.btnRecord.UseVisualStyleBackColor = true;
+            this.btnRecord.Click += new System.EventHandler(this.btnRecord_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Image = global::bBear_v1.Properties.Resources.building64;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnDashboard
+            // 
+            this.btnDashboard.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDashboard.FlatAppearance.BorderSize = 0;
+            this.btnDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDashboard.ForeColor = System.Drawing.Color.Black;
+            this.btnDashboard.Image = global::bBear_v1.Properties.Resources.Report32;
+            this.btnDashboard.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDashboard.Location = new System.Drawing.Point(10, 0);
+            this.btnDashboard.Name = "btnDashboard";
+            this.btnDashboard.Size = new System.Drawing.Size(184, 63);
+            this.btnDashboard.TabIndex = 0;
+            this.btnDashboard.Text = "Dashboard";
+            this.btnDashboard.UseVisualStyleBackColor = true;
+            // 
+            // userControlFile1
+            // 
+            this.userControlFile1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.userControlFile1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.userControlFile1.Location = new System.Drawing.Point(200, 0);
+            this.userControlFile1.Name = "userControlFile1";
+            this.userControlFile1.Size = new System.Drawing.Size(684, 611);
+            this.userControlFile1.TabIndex = 1;
+            // 
+            // userControlRecord1
+            // 
+            this.userControlRecord1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.userControlRecord1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.userControlRecord1.Location = new System.Drawing.Point(200, 0);
+            this.userControlRecord1.Name = "userControlRecord1";
+            this.userControlRecord1.Size = new System.Drawing.Size(684, 611);
+            this.userControlRecord1.TabIndex = 2;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.ClientSize = new System.Drawing.Size(884, 611);
+            this.Controls.Add(this.userControlRecord1);
+            this.Controls.Add(this.userControlFile1);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormMain";
+            this.Text = "FormMain";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel8.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel7.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnDashboard;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Button btnReport;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button btnInvoice;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Button btnRecipt;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnFile;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button btnRecord;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Button btnSetting;
+        private UserControlFile userControlFile1;
+        private UserControlRecord userControlRecord1;
+    }
+}
+
---
+++ bBear_v1/FormMain.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bBear_v1
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+            userControlFile1.Visible = false;
+            userControlRecord1.Visible = false;
+        }
+
+        //private void FormMain_Load(object sender, EventArgs e)
+        //{
+        //    // Hide UserControls initially
+        //    userControlFile1.Visible = false;
+        //    userControlRecord1.Visible = false;
+        //}
+
+        private void btnFile_Click(object sender, EventArgs e)
+        {
+            // Show UserControlFile and hide UserControlRecord
+            userControlFile1.Visible = true;
+            userControlRecord1.Visible = false;
+        }
+
+        private void btnRecord_Click(object sender, EventArgs e)
+        {
+            // Show UserControlRecord and hide UserControlFile
+            userControlFile1.Visible = false;
+            userControlRecord1.Visible = true;
+        }
+    }
+}
